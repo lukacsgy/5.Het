d92dfe337f7f256b2291d8c67a4ffe628d23b7ed
György Lukács <georgelucasletters@gmail.com>
2018-01-15 09:04:35 +0100

    Nem fut le....de elmeletben szerintem jo

--- Szamkitalaloforditva/main.c
+++ Szamkitalaloforditva/main.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(void){
+    int szam, tip;
+    char valasz;
 
-    printf("Hello world!\n");
+    printf("Add meg a szamot 1-100 kozott: ");
+    scanf("%d", &szam);
+
+    srand(time(0));
+    tip = rand()%10+1;
+
+    do{
+        printf("A szam %d-nél kissebb? (I)gen vagy (N)em", tip);
+        scanf(" c%", &valasz);
+
+        switch(valasz){
+            case 'I':
+            case 'i':
+                tip = rand()tip+1;
+                break;
+            case 'N':
+            case 'n':
+                tip = rand()%10+tip;
+                break;
+        }
+    }while(tip != szam);
+    Printf("Kitalaltam a szam amire gondoltal: %d", tip);
+
     return 0;
 }
