9b578f675dd10a46f65e69f601c8a2e41610021e
György Lukács <georgelucasletters@gmail.com>
2018-01-16 15:29:31 +0100

    Írj függvényt, amely hatványozást végez egész számokon!

--- Armstrongszamok/main.c
+++ Armstrongszamok/main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int szamjegy(int szam){
+    int db = 0;
+
+    while(szam > 0){
+        szam = szam / 10;
+        db++;
+    }
+    return(db);
+}
+
+int hatvany(int szam, int kitevo){
+    int i, eredmeny = 1;
+
+    for(i = 0; i < kitevo; i++){
+        eredmeny *= szam;
+    }
+
+    return(eredmeny);
+}
 int main()
 {
     printf("Hello world!\n");
