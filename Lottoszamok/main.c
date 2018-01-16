505a4e2bb98883dd129c599db2d7c20ff76f2713
György Lukács <georgelucasletters@gmail.com>
2018-01-16 14:06:32 +0100

    Szerintem igy jo... szamot 0ra atirom azt csobabineni!

--- Lottoszamok/main.c
+++ Lottoszamok/main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <time.h>
 int main(void){
+    int i, index, lotto[90];
 
-    printf("Hello world!\n");
+    for (i = 0; i < 90; i++){
+        lotto[i]= i + 1;
+    }
+    srand(time(0));
+
+    for (i = 0; i < 5; i++){
+        do{
+            index = rand() %90 + 1;
+
+
+        }while(lotto[index] == 0);
+
+        printf("%d, ", lotto[index]);
+        lotto[index] = 0;
+    }
+
     return 0;
 }
