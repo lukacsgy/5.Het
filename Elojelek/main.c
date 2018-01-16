601b4c6404cc5accf307e9eb2af6b4286d0e8f92
György Lukács <georgelucasletters@gmail.com>
2018-01-16 14:46:56 +0100

    Megcsinaltam de szerintem elég darabos lett

--- Elojelek/main.c
+++ Elojelek/main.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-int main()
-{
-    printf("Hello world!\n");
+bool elojel(int szam1, int szam2){
+    bool egyezik;
+    if (szam1 > 0 && szam2 > 0 || szam1 < 0 && szam2 < 0)
+        egyezik = true;
+
+    return (egyezik);
+
+}
+
+int main(void){
+    int szam1, szam2;
+
+    printf("Add meg az elso szamot: ");
+    scanf("%d", &szam1);
+    printf("Add meg a masodik szamot: ");
+    scanf("%d", &szam2);
+
+    bool egyezik = elojel(szam1,szam2);
+
+    if(egyezik){
+        printf("A 2 szam azaonos elojelu");
+    }
+    else{
+        printf("A 2 szam elojele nem egyezik");
+    }
+
     return 0;
 }
