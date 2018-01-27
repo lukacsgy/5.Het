ea57044f335c100549ab02e2d54cfcc460552f09
György Lukács <georgelucasletters@gmail.com>
2018-01-27 14:38:36 +0100

    Done & adam segitett mert megint tipusokkal rosszul bantam

--- kartyapakliI/main.c
+++ kartyapakliI/main.c
@@ -1,11 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
 typedef struct {
-    char szin, szama;
-}kartya;
+    int szin, szama;
+} Kartya;
+
+Kartya uj_lap(char szin, char szam){
+    Kartya lap;
+    lap.szin = szin;
+    lap.szama = szam;
+    return lap;
+}
 
 int main(void){
+    char szinek[4] = {'p', 'k', 't', 's'};
+    char szamok[13] = {'2', '3', '4', '5', '6', '7','8','9','0','J','Q','K','A'};
 
-    printf("Hello world!\n");
+    Kartya pakli[52];
+
+    int i, j;
+    for (j = 0; j < 4; j++){
+        for(i = 0; i < 13; i++){
+            pakli[j * 13 + i] = uj_lap(j, i);
+        }
+    }
+
+    srand(time(0));
+    Kartya csere;
+    int rnd;
+
+    for (i = 0; i < 52; i++){
+        rnd = rand() % 52;
+        if ( i > rnd){
+            csere = pakli[i];
+            pakli[i] = pakli[rnd];
+            pakli[rnd] = csere;
+        }
+    }
+
+    for(i = 0; i < 52; i++){
+
+        printf("%c%c, ", szinek[pakli[i].szin], szamok[pakli[i].szama]);
+    }
+
     return 0;
 }
